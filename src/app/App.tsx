709a11c1f4df48ae0c0e6dc9332d620cd68d9ff0
cezar <cezarcqc@gmail.com>
2020-11-25 16:17:47 -0300

    tarefas a fazer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { Component } from "react";
+import Tarefas from "../component/Tarefas";
 
 function App() {
   const [tech, setTech] =  useState(['um', 'dois', 'feijao','com','arroz']);
@@ -11,12 +13,20 @@ function App() {
     setTech([...tech, newTech]);
     setNewTech('');
   }
+  useEffect(() => {
+    const storageTech = localStorage.getItem('tech');
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+  }, []);
   return (
     <div className="App">
       <h1>Hello Code Challenge!</h1>
+      <Tarefas/>
       <div> {tech.map(t => (
           <li key={t}>{t}</li>
         ))}</div>
+        
       <input type="text" onFocus={usaEstado} value={newTech}  onChange={e  =>  setNewTech(e.target.value)}  />
     </div>
   );
