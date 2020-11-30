49402b325e8213e469fe1ef5879041f375ccd669
cezar <cezarcqc@gmail.com>
2020-11-30 15:34:42 -0300

    refazendo toda a estrutura

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,37 +1,51 @@
+import { setegid } from 'process';
 import React, { useState } from 'react';
 import { Component } from "react";
 import Tarefas from "../component/Tarefas";
+import ListItem from '../component/ListItem'
 
+  interface IntrinsicElements {
+      'onchange': any
+  }
 function App() {
-  let [tech, setTech] =  useState(['um', 'dois', 'feijao','com','arroz']);
-  const [newTech, setNewTech] =  useState('');
+  let [tech, setTech] =  useState([{index: 1 ,name:'um'},{index: 2 ,name: 'dois'},{index: 1 ,name:"feijao"} ]);
+  const [newTech, setNewTech] : any =  useState();
+  
 
-  function usaEstado(){
-    setTech([...tech, 'tres', 'quatro','feijao','no prato']);
+
+  function handleUpdate(index: number) {
+    const itensCopy : any  = Array.from(tech);
+    itensCopy.splice(index, 1, { id: index });
+    setTech(itensCopy);
   }
-  function handleAdd() {
-    setTech([...tech, newTech]);
-    setNewTech('');
+  function handleAdd() {    
+    const itensCopy : any = Array.from(tech);
+    itensCopy.push({id: newTech.length, value: tech});
+    setTech(itensCopy);
   }
-  function handleDelete(){
-    setTech([...tech, tech[1]]);
+  function handleDelete(index: number ) {
+    const itensCopy = Array.from(tech);
+    itensCopy.splice(index, 1);
+    setTech(itensCopy);
   }
-  useEffect(() => {
-    const storageTech = localStorage.getItem('tech');
-    if (storageTech) {
-      setTech(JSON.parse(storageTech));
-    }
-  }, []);
+ // useEffect(() => {
+  //  const storageTech = localStorage.getItem('tech');
+  //  if (storageTech) {
+  //    setTech(JSON.parse(storageTech));
+  //  }
+ // }, []);
   return (
     <div className="App">
       <h1>Hello Code Challenge!</h1>
-      <Tarefas/>
-      <div> {tech.map(t => (
-          <li key={t}>{t}</li>
+      
+      <div id="42" className="listagem"> {tech.map( index => (
+          <Tarefas onChange = {(event) => handleUpdate(event, index)}
+            onDelete = {() => handleDelete(index)}></Tarefas>
         ))}</div>
 
         
-      <button type="text" onClick={usaEstado} value={newTech}  onChange={e  =>  setNewTech(e.target.value)}  />
+      <button type="button" onClick={handleAdd} value={newTech} onBlur={usaEstado} >mais</button>
+      <button type="button"  onClick={handleDelete} onBlur={usaEstado} > delete </button>
     </div>
   );
 }
--- src/component/Tarefas.js
+++ src/component/Tarefas.js
@@ -1,9 +1,31 @@
-import React, { Component } from "react";
+import React, { useState } from 'react';
+const Tarefas = ({ onSubmit }) => {
 
+  const [newItem, setNewItem] = useState('');
 
+  function setNewTask({target}) {
+    setNewItem(target.value);
+  }
 
-export default class extends Component{
-    render(){
-        return <h2>cinco</h2>
-    }
-}
+  function submit(e) {
+    e.preventDefault();
+    onSubmit(newItem);
+  }
+
+  return (
+    <div>
+      <form onSubmit={submit}>
+        <input
+          className="Todo-input"
+          placeholder="Digite"
+          onChange={setNewTask}
+        />
+        <button type="submit">
+          Adicionar
+        </button>
+      </form>
+    </div>
+  )
+};
+
+export default Tarefas;
